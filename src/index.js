44eb7b28a9ec8fee28f875f4e226f653595ebc58
Napster <rome.1205h@gmail.com>
2020-05-15 19:41:51 -0500

    agregando cache-control  15-05

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use((req, res, next) => {
+    const cacheTime = 60*60*24;// 60 segundos * 60 minutos * 24 horas = 1 día
+    res.set({
+        'Cache-Control': `max-age=${cacheTime}`
+    });
+    next();
+});
 
 // routes
 app.use(require('./routes/index'));
